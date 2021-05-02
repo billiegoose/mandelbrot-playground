b0f51fbf23188b305958ce05a74bf16e98ad8224
William Hilton <wmhilton@gmail.com>
2021-05-02 15:49:52 -0400

    remove fullscreen polyfill since fullscreen is handled by HEN

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Mandelbrot set viewer
 
-This is an interactive Mandelbrot set viewer! Supported features:
-- panning (click and drag or touch and drag)
-- zooming (scroll wheel or pinch zoom)
-- toggle fullscreen (double click or double tap)
-- download current image as PNG (click/tap "camera" icon)
+This is an interactive Mandelbrot set viewer!
+To PAN, click (touch) and drag.
+To ZOOM, use mouse scroll wheel (pinch zoom on multitouch).
+To SAVE the current image, right click (long press) on the camera icon in the top right corner and choose "open link in new tab".
+One of seven different starting locations is chosen randomly each time you load the page.
--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-// POLYFILLS
-// oh safari, bane of life
-if (typeof document.fullscreenEnabled === 'undefined' && typeof document.webkitFullscreenEnabled === 'boolean') {
-  // Polyfill the dang things
-  Object.defineProperties(document, {
-    fullscreenEnabled: {
-      get: function () {
-        return this.webkitFullscreenEnabled;
-      },
-    },
-    fullscreenElement: {
-      get: function () {
-        return this.webkitFullscreenElement;
-      },
-    },
-    exitFullscreen: {
-      get: function () {
-        return this.webkitExitFullscreen;
-      },
-    },
-  });
-  Object.defineProperties(Element.prototype, {
-    requestFullscreen: {
-      get: function () {
-        return this.webkitRequestFullscreen;
-      },
-    },
-  });
-}
-
 // UTILITY FUNCTIONS
 
 const add = (c1, c2) => ({ r: c1.r + c2.r, i: c1.i + c2.i });
@@ -429,14 +399,4 @@ canvas.addEventListener(
 canvas.addEventListener("pointerup", event => {
   delete pointers[event.pointerId];
   updateSaveLink();
-});
-
-canvas.addEventListener('dblclick', () => {
-  if (!document.fullscreenEnabled) return;
-
-  if (document.fullscreenElement) {
-    document.exitFullscreen();
-  } else {
-    canvas.requestFullscreen();
-  }
 });
